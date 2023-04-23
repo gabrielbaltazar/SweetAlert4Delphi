9e9fb7aa907c6bbe65ca46690b402daf9e967261
Gabriel Baltazar <gabrielbaltazar.f1@gmail.com>
2023-04-23 19:53:26 -0300

    Sweet Tests

--- Sample/Intraweb/BinSweets/wwwroot/js/sweetalert.custom.js
+++ Sample/Intraweb/BinSweets/wwwroot/js/sweetalert.custom.js
@@ -2,13 +2,19 @@ function ShowSweetMessage(message) {
     Swal.fire(message);
 }
 
-function SweetError(title, text) {
-    let message = {
-        icon: 'error',
-        showCloseButton: true,
-        title,
-        text
-    };
+// function SweetError(title, text) {
+//     let message = {
+//         icon: 'error',
+//         showCloseButton: true,
+//         title,
+//         text
+//     };
 
-    Swal.fire(message);
+//     Swal.fire(message);
+// }
+
+function SweetError(message) {
+    let sweetMessage = message;
+    sweetMessage.icon = 'error',
+    ShowSweetMessage(sweetMessage);
 }
